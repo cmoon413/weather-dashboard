c22e06641da4657607903819d0af357bc6a0b0d2
Chester Moon <cmoon3740@gmail.com>
2020-02-09 16:21:49 -0800

    current weather url function created

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,21 @@
 
 //variables
 var cities = []
-
-
-//adding listener to search button
+var APIKey = "cbe32bb3b579dad365829cdc5ba21e51"
+var currentURL
+    //adding listener to search button
 $("#search").on("click", function(event) {
     event.preventDefault()
     cities.push($('#cityInput').val().trim())
     $('#cityInput').val('')
-
+    currentWeatherURL(cities[0])
 })
+
+
+
+
+
+//creates a url for the current weather
+function currentWeatherURL(city) {
+    currentURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey
+}
