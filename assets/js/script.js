a51a8f98814dad16f47c981508a0411f63370f6c
Chester Moon <cmoon3740@gmail.com>
2020-02-10 20:20:24 -0800

    pulled display work out of click event and into own function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,28 +5,31 @@ var cities = []
 var APIKey = "cbe32bb3b579dad365829cdc5ba21e51"
 var currentURL
 var forecastURL
+var UvURL
 var weatherForecast = []
     //adding listener to search button
 $("#search").on("click", function(event) {
-    event.preventDefault()
-    cities.unshift($('#cityInput').val().trim())
-    $('#cityInput').val('')
+        event.preventDefault()
+        cities.push($('#cityInput').val().trim())
+        $('#cityInput').val('')
+        displayWeather(cities[cities.length - 1])
+    })
+    //displays weather forecast
+function displayWeather(city) {
     renderCities()
-    createCurrentURL(cities[0])
+    createCurrentURL(city)
     getCurrent()
-    createForecastURL(cities[0])
+    createForecastURL(city)
     getForecast()
-})
+}
 
 
 
-
-
-//creates a url for the current weather
+//Functions to create URLS for API calls
 function createCurrentURL(city) {
     currentURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey
 }
-//creates a url for forecast
+
 function createForecastURL(city) {
     forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=cbe32bb3b579dad365829cdc5ba21e51"
 }
@@ -41,7 +44,6 @@ function getCurrent() {
         var lat = response.coord.lat
         var lon = response.coord.lon
 
-        console.log(response)
         $('.card-title').text(cities[0])
         $('#temp').text('Temperature: ' + response.main.temp)
         $('#humidity').text('Humidity: ' + response.main.humidity)
@@ -63,8 +65,6 @@ function getForecast() {
         var forecast = document.querySelectorAll('.weather-forecast')
         console.log(response)
         for (let i = 7; i < 40; i += 8) {
-            console.log(response.list[i].weather[0].icon)
-
             forecast[count].previousElementSibling.innerHTML = moment.unix(response.list[i].dt).format("MM/DD/YYYY")
             forecast[count].children[0].src = 'http://openweathermap.org/img/wn/' + response.list[i].weather[0].icon + '.png'
             forecast[count].children[1].innerHTML = 'Temp: ' + response.list[i].main.temp
@@ -79,6 +79,6 @@ function renderCities() {
     for (var i = 0; i < cities.length; i++) {
         var city = $('<li>').addClass('collection-item').attr('data-city', cities[i]).text(cities[i])
         var a = $("<button>");
-        $("#cities").append(city)
+        $("#cities").prepend(city)
     }
 }
